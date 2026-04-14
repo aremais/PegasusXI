b7434031abe4d5e282fdabd7230ef2e16a4a4ee5
Faigai <weston.fulmer@gmail.com>
2026-04-14 16:26:38 -0400

    check clear maybe

--- scripts/globals/player_job_levels.lua
+++ scripts/globals/player_job_levels.lua
@@ -23,7 +23,7 @@ xi.player_job_levels = xi.player_job_levels or {}
 function xi.player_job_levels.setAllJobLevels(player, level)
     level = level or 99
     if level < 1 or level > 99 then
-        return false, "Level must be between 1 and 99."
+        return false, 'Level must be between 1 and 99.'
     end
 
     local originalJob = player:getMainJob()
