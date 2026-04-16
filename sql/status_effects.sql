153c6b1ebbc2a58c346aa1ba137bf45273453319
Faigai <weston.fulmer@gmail.com>
2026-04-16 17:18:28 -0400

    removed effects for failed aura

--- sql/status_effects.sql
+++ sql/status_effects.sql
@@ -707,7 +707,6 @@ INSERT INTO `status_effects` VALUES (802,'elemental_resistance_down',@FLAG_DEATH
 INSERT INTO `status_effects` VALUES (803,'full_speed_ahead',@FLAG_ON_ZONE | @FLAG_NO_LOSS_MESSAGE,0,0,0,0,0,0,0,0,NULL);
 INSERT INTO `status_effects` VALUES (805,'tomahawk',@FLAG_DEATH | @FLAG_NO_LOSS_MESSAGE,0,0,0,0,0,0,0,0,NULL);
 INSERT INTO `status_effects` VALUES (806,'nuke_wall',@FLAG_DEATH | @FLAG_ON_ZONE,0,0,0,0,0,0,0,0,NULL);
-INSERT INTO `status_effects` VALUES (807,'kupofried_aura',@FLAG_DEATH | @FLAG_ON_ZONE,247,0,0,0,0,0,0,0,NULL);
 
 
 /*!40000 ALTER TABLE `status_effects` ENABLE KEYS */;
