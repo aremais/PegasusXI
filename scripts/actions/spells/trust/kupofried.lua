2af72b2c3221c8911d8a10e704cf158fa522d890
Faigai <weston.fulmer@gmail.com>
2026-04-19 21:54:39 -0400

    Kupo geo dedication fix

--- scripts/actions/spells/trust/kupofried.lua
+++ scripts/actions/spells/trust/kupofried.lua
@@ -1,6 +1,8 @@
 -----------------------------------
 -- Trust: Kupofried
+-- Passive aura: grants EXP/CP bonus to nearby party members
 -----------------------------------
+
 ---@type TSpellTrust
 local spellObject = {}
 
@@ -14,6 +16,24 @@ end
 
 spellObject.onMobSpawn = function(mob)
     xi.trust.message(mob, xi.trust.messageOffset.SPAWN)
+    local mlvl = mob:getMainLvl()
+    local tick_amount
+    if mlvl >= 99 then
+    tick_amount = 600
+elseif mlvl >= 87 then
+    tick_amount = 500
+elseif mlvl >= 73 then
+    tick_amount = 400
+elseif mlvl >= 51 then
+    tick_amount = 300
+elseif mlvl >= 25 then
+    tick_amount = 200
+else
+    tick_amount = 100
+end
+
+    mob:addStatusEffect(xi.effect.COLURE_ACTIVE, { power = 6, origin = mob, tick = 3, subType = xi.effect.CORSAIRS_ROLL, subPower = tick_amount, tier = xi.auraTarget.ALLIES, flag = xi.effectFlag.AURA })
+    mob:setAutoAttackEnabled(false)
 end
 
 spellObject.onMobDespawn = function(mob)
