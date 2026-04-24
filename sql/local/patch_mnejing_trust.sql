e036f3db3c9ec454fd08c2ef72ee19bd8d39629a
tragic-win <bbills91@gmail.com>
2026-04-23 19:51:57 -0600

    Remove local Mnejing SQL patch files

--- sql/local/patch_mnejing_trust.sql
+++
@@ -1,8 +0,0 @@
--- Enable Mnejing trust skill list
-
-UPDATE mob_pools
-SET skill_list_id = 1041
-WHERE poolid = 5926;
-
-INSERT INTO mob_skill_lists
-VALUES ('TRUST_Mnejing', 1041, 0);
--- sql/local/verify_mnejing_trust.sql
+++
@@ -1,7 +0,0 @@
-SELECT poolid, name, packet_name, mJob, sJob, cmbSkill, spellList, skill_list_id
-FROM mob_pools
-WHERE poolid = 5926;
-
-SELECT *
-FROM mob_skill_lists
-WHERE skill_list_id = 1041;
