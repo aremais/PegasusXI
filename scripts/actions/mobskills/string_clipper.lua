ebce2eb4ff01141d663597d0a1e2199bb5a61ca0
tragic-win <bbills91@gmail.com>
2026-04-26 13:57:16 -0600

    Update Mnejing mobskills for physical helper signature

--- scripts/actions/mobskills/string_clipper.lua
+++ scripts/actions/mobskills/string_clipper.lua
@@ -11,7 +11,7 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
     return 0
 end
 
-mobskillObject.onMobWeaponSkill = function(target, mob, skill)
+mobskillObject.onMobWeaponSkill = function(target, mob, skill, action)
     local accMod = 0
 
     if skill:getTP() >= 3000 then
@@ -23,20 +23,20 @@ mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local params =
     {
         numHits = 2,
-        ftpMod = { 3.5, 3.5, 3.5 },
-        str_wsc = 0.30,
-        dex_wsc = 0.30,
-        accMod = accMod,
-        atkVaries = { 1.25, 1.25, 1.25 },
+        fTP = { 3.5, 3.5, 3.5 },
+        str_wSC = 0.30,
+        dex_wSC = 0.30,
+        accuracyModifier = { accMod, accMod, accMod },
+        attackMultiplier = { 1.25, 1.25, 1.25 },
         skill = xi.skill.NONE,
     }
 
-    local damage = xi.mobskills.mobPhysicalMove(mob, target, skill, params)
-    damage = xi.mobskills.mobFinalAdjustments(damage.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, 1)
+    local damage = xi.mobskills.mobPhysicalMove(mob, target, skill, action, params)
+    damage.damage = xi.mobskills.mobFinalAdjustments(damage.damage, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, damage.hitsLanded)
 
-    target:takeDamage(damage, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
+    target:takeDamage(damage.damage, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
 
-    return damage
+    return damage.damage
 end
 
 return mobskillObject
