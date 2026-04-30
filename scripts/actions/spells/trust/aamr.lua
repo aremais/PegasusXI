c2e69ec8fe6cf0ee83f76d40944f85ee50567af5
tragic-win <bbills91@gmail.com>
2026-04-30 15:07:27 -0600

    Fix AAMR trust Lua sanity checks

--- scripts/actions/spells/trust/aamr.lua
+++ scripts/actions/spells/trust/aamr.lua
@@ -4,10 +4,6 @@
 ---@type TSpellTrust
 local spellObject = {}
 
-local wsRampage       = 3715
-local wsCalamity      = 3716
-local wsHavocSpiral   = 3717
-local wsCloudsplitter = 3718
 
 spellObject.onMagicCastingCheck = function(caster, target, spell)
     return xi.trust.canCast(caster, spell)
@@ -19,29 +15,24 @@ end
 
 spellObject.onMobFight = function(mob, target)
     if
-        mob:getLocalVar('aamrSneakAttack') < os.time() and
+        mob:getLocalVar('aamrSneakAttack') < GetSystemTime() and
         mob:getTP() >= 1000
     then
         mob:useJobAbility(xi.ja.SNEAK_ATTACK, mob)
-        mob:setLocalVar('aamrSneakAttack', os.time() + 60)
+        mob:setLocalVar('aamrSneakAttack', GetSystemTime() + 60)
     end
 
     if
-        mob:getLocalVar('aamrTrickAttack') < os.time() and
+        mob:getLocalVar('aamrTrickAttack') < GetSystemTime() and
         mob:getTP() >= 1000
     then
         mob:useJobAbility(xi.ja.TRICK_ATTACK, mob)
-        mob:setLocalVar('aamrTrickAttack', os.time() + 60)
+        mob:setLocalVar('aamrTrickAttack', GetSystemTime() + 60)
     end
 end
 
 spellObject.onMobSpawn = function(mob)
     xi.trust.message(mob, xi.trust.messageOffset.SPAWN)
-
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.TP_GTE, 3000, ai.r.WS, wsHavocSpiral, ai.s.TARGET)
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.TP_GTE, 2000, ai.r.WS, wsCloudsplitter, ai.s.TARGET)
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.TP_GTE, 1500, ai.r.WS, wsRampage, ai.s.TARGET)
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.TP_GTE, 1000, ai.r.WS, wsCalamity, ai.s.TARGET)
 end
 
 spellObject.onMobDespawn = function(mob)
