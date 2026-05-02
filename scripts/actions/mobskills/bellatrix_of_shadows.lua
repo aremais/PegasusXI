95d7f0ac4570a20c0709c97894c687418c9da27c
tragic-win <bbills91@gmail.com>
2026-05-02 11:39:20 -0600

    Adjust Arciela combat behavior

--- scripts/actions/mobskills/bellatrix_of_shadows.lua
+++ scripts/actions/mobskills/bellatrix_of_shadows.lua
@@ -1,5 +1,6 @@
 -----------------------------------
 -- Bellatrix of Shadows
+-- Physical damage.
 -----------------------------------
 ---@type TMobSkill
 local mobskillObject = {}
@@ -9,8 +10,19 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
 end
 
 mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
-    -- Stance/transition action. No direct combat effect here.
-    return 0
+    local params =
+    {
+        numHits = 1,
+        ftpMod  = { 1.5, 2.0, 2.5 },
+        str_wSC = 0.3,
+        dex_wSC = 0.3,
+        shadowBehavior = xi.mobskills.shadowBehavior.WIPE_SHADOWS,
+    }
+
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, action, params)
+    xi.mobskills.processDamage(mob, target, skill, action, info)
+
+    return info.damage
 end
 
 return mobskillObject
