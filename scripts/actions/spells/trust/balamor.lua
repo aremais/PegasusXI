7b3fc55122cceb7440f3755c2426da5be2cb5ba8
tragic-win <bbills91@gmail.com>
2026-05-05 16:43:32 -0600

    Update Balamor trust behavior

--- scripts/actions/spells/trust/balamor.lua
+++ scripts/actions/spells/trust/balamor.lua
@@ -14,6 +14,9 @@ end
 
 spellObject.onMobSpawn = function(mob)
     xi.trust.message(mob, xi.trust.messageOffset.SPAWN)
+
+    -- Balamor special moves from skill list 1098.
+    mob:addGambit(ai.t.TARGET, { ai.c.TP_GTE, 1000 }, { ai.r.WS, ai.s.HIGHEST, 0 })
 end
 
 spellObject.onMobDespawn = function(mob)
