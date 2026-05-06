07bc1572c84ae77acaed6f16e328887229280bb9
Faigai <weston.fulmer@gmail.com>
2026-05-06 17:09:46 -0400

    BST Jug Ready Function
    reversing a screw up, and updating problem ready moves.

--- scripts/actions/abilities/pets/molting_plumage.lua
+++ scripts/actions/abilities/pets/molting_plumage.lua
@@ -4,7 +4,7 @@
 -----------------------------------
 ---@type TAbilityPet
 local abilityObject = {}
-local skillName = 'choke_breath'
+local skillName = 'molting_plumage'
 
 abilityObject.onAbilityCheck = function(player, target, ability)
     return 0
--- scripts/actions/abilities/pets/sweeping_gouge.lua
+++ scripts/actions/abilities/pets/sweeping_gouge.lua
@@ -4,7 +4,7 @@
 -----------------------------------
 ---@type TAbilityPet
 local abilityObject = {}
-local skillName = 'choke_breath'
+local skillName = 'sweeping_gouge'
 
 abilityObject.onAbilityCheck = function(player, target, ability)
     return 0
