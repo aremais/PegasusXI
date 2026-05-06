0988c31720c29aeeae51ab5ff330dede92dc2faf
tragic-win <bbills91@gmail.com>
2026-05-06 15:08:57 -0600

    Fix Balamor trust mob skill usage

--- scripts/actions/spells/trust/balamor.lua
+++ scripts/actions/spells/trust/balamor.lua
@@ -15,8 +15,14 @@ end
 spellObject.onMobSpawn = function(mob)
     xi.trust.message(mob, xi.trust.messageOffset.SPAWN)
 
-    -- Balamor special moves from skill list 1098.
-    mob:addGambit(ai.t.TARGET, { ai.c.TP_GTE, 1000 }, { ai.r.WS, ai.s.HIGHEST, 0 })
+    mob:setMobMod(xi.mobMod.TRUST_DISTANCE, xi.trust.movementType.MELEE)
+    mob:setMobMod(xi.mobMod.SKILL_LIST, 1098)
+
+    -- Balamor special mob skills.
+    mob:addGambit(ai.t.TARGET, { ai.c.TP_GTE, 1500 }, { ai.r.MS, ai.s.SPECIFIC, 3620 }, 30) -- Last Laugh
+    mob:addGambit(ai.t.TARGET, { ai.c.TP_GTE, 1250 }, { ai.r.MS, ai.s.SPECIFIC, 3619 }, 25) -- Setting the Stage
+    mob:addGambit(ai.t.TARGET, { ai.c.TP_GTE, 1000 }, { ai.r.MS, ai.s.SPECIFIC, 3618 }, 20) -- Regurgitated Swarm
+    mob:addGambit(ai.t.TARGET, { ai.c.TP_GTE, 1000 }, { ai.r.MS, ai.s.SPECIFIC, 3617 }, 15) -- Feast of Arrows
 end
 
 spellObject.onMobDespawn = function(mob)
