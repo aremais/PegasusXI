45cc374b3f290a090898b95c3b3e53f576dc7687
tragic-win <bbills91@gmail.com>
2026-05-26 18:14:31 -0600

    Probe Nolan event menu

--- scripts/zones/Norg/npcs/Nolan.lua
+++ scripts/zones/Norg/npcs/Nolan.lua
@@ -1,27 +1,28 @@
 -----------------------------------
 -- Area: Norg
 --  NPC: Nolan
--- Type: Escha Bead Exchange
------------------------------------
-require('scripts/globals/nolan_shop')
+-- Type: Event Probe
 -----------------------------------
 ---@type TNpcEntity
 local entity = {}
 
+local probeEvent = 9512
+
 entity.onTrade = function(player, npc, trade)
-    xi.nolanShop.onTrade(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    xi.nolanShop.onTrigger(player, npc)
+    player:printToPlayer(string.format('Testing Nolan event %u.', probeEvent), xi.msg.channel.SYSTEM_3)
+    player:startEvent(probeEvent, player:getCurrency('escha_beads'), player:getCurrency('escha_silt'))
 end
 
 entity.onEventUpdate = function(player, csid, option, npc)
-    xi.nolanShop.onEventUpdate(player, csid, option, npc)
+    player:printToPlayer(string.format('Nolan event update: csid=%u option=%u.', csid, option), xi.msg.channel.SYSTEM_3)
+    player:updateEvent(player:getCurrency('escha_beads'), player:getCurrency('escha_silt'))
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    xi.nolanShop.onEventFinish(player, csid, option, npc)
+    player:printToPlayer(string.format('Nolan event finish: csid=%u option=%u.', csid, option), xi.msg.channel.SYSTEM_3)
 end
 
 return entity
